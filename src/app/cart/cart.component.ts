525eaf10c7feff11caa983eec74ce4359ea27da2
temi-tope <temitope.ogungbesan.5@gmail.com>
2019-12-03 16:56:02 +0100

    forms

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import {CartService} from '../cart.service';
 
 @Component({
@@ -8,14 +9,26 @@ styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
 items;
+checkoutForm;
 
 constructor(
-  private cartService: CartService
-) { }
-
-// tslint:disable-next-line: use-lifecycle-interface
-ngOnInit() {
+  private cartService: CartService,
+  private formBuilder: FormBuilder,
+) {
   this.items = this.cartService.getItems();
+
+  this.checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 }
+
+onSubmit(customerData) {
+  console.warn('Your order has been submitted', customerData);
+
+  this.items = this.cartService.clearCart();
+  this.checkoutForm.reset();
+}
+// tslint:disable-next-line: use-lifecycle-interface
 
 }
